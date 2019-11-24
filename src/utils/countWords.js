0dbf0c7052828db9dc162fe89494ac61674cd7a2
Simon Opheim <simon.opheim@attentec.no>
2019-11-24 19:16:48 +0100

    fix lint

--- src/utils/countWords.js
+++ src/utils/countWords.js
@@ -4,6 +4,7 @@ export default function countWords(string) {
   const words = wordsRaw.map(word => word.replace(/[.,/#!$%^&*;:{}=\-_`'´~()?]/g, '')).filter(word => word.length > 0);
   const count = {};
 
+  // eslint-disable-next-line no-unused-expressions
   words.forEach((word) => { !count[word] ? count[word] = 1 : count[word] += 1; });
 
   return count;
